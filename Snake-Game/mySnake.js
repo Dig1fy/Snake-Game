a5ca48db558a57fc7640968239dbea8b19444ee0
Q2kPetrov <ivaylozp@gmail.com>
2020-08-30 18:56:46 +0300

    Refactoring + better apple appearance

--- Snake-Game/mySnake.js
+++ Snake-Game/mySnake.js
@@ -8,24 +8,26 @@ let domElements = {
 }
 
 let foodImage = new Image();
-foodImage.src = "images/snakeFood-modified.png";
-// foodImage.style.width = "20px";
-// foodImage.style.height = "20px";
+foodImage.src = "images/modifiedApple.png";
 
 let Directions = {
-    up: 1, down: 2, left: 3, right: 4
+    up: 1, 
+    down: 2, 
+    left: 3, 
+    right: 4
 }
 
 let GameState =
 {
-    Start: 1, Playing: 2, GameOver: 3
+    Start: 1, 
+    Playing: 2, 
+    GameOver: 3
 }
 
 let Game = {
     score: 0,
     context: undefined,
     state: GameState.Playing,
-
     blockSize: {
         width: 25,
         height: 25
@@ -92,15 +94,17 @@ function updateGameState(e) {
 
 }
 
+//Reset all the data and set the initial snake position
 function startGame() {
-    //First, we need to reset all the data and set the initial snake position
     Game.score = 0;
     Game.food = undefined;
-    Snake.headPosition = { x: 15, y: 1 }
+    Snake.headPosition = { x: 2, y: 1 }
     Snake.direction = Directions.right;
     Snake.body = [
-        { x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }, { x: 3, y: 1 }, { x: 4, y: 1 }, { x: 5, y: 1 }, { x: 6, y: 1 }, { x: 7, y: 1 },
-        { x: 8, y: 1 }, { x: 9, y: 1 }, { x: 10, y: 1 }, { x: 11, y: 1 }, { x: 12, y: 1 }, { x: 13, y: 1 }, { x: 14, y: 1 }, { x: 15, y: 1 }
+        { x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }
+        //For testing purposes
+        // , { x: 3, y: 1 }, { x: 4, y: 1 }, { x: 5, y: 1 }, { x: 6, y: 1 }, { x: 7, y: 1 },
+        // { x: 8, y: 1 }, { x: 9, y: 1 }, { x: 10, y: 1 }, { x: 11, y: 1 }, { x: 12, y: 1 }, { x: 13, y: 1 }, { x: 14, y: 1 }, { x: 15, y: 1 }
     ]
 
     let interval = window.setInterval(() => {
@@ -145,8 +149,6 @@ function checkSnakeDirection() {
             } else if (e.code === "ArrowUp" && Snake.direction !== Directions.down) {
                 Snake.newDirection = Directions.up;
             }
-
-            // console.log("1");
         }
     })
 }
@@ -154,9 +156,9 @@ function checkSnakeDirection() {
 function changeSnakePosition() {
     if (Snake.newDirection) {
         Snake.direction = Snake.newDirection;
+
         //Once we've updated the next direction, we set it to undefined, waiting for new direction from the listener
         Snake.newDirection = undefined;
-
     }
 
     let sX = Snake.headPosition.x;
@@ -176,7 +178,6 @@ function changeSnakePosition() {
     //We add new head block and remove one block from the tail
     Snake.body.push(Snake.headPosition);
     Snake.body.shift();
-    console.log("2");
 }
 
 //We check both scenarios - hitting itself or going outside the game area.
@@ -187,11 +188,10 @@ function checkIfSnakeDies() {
     //if snake hits left wall / right wall / bottom / top
     if (x < 0 || x >= Game.blockSize.width - 2 || y < 0 || y >= Game.blockSize.height - 2) {
         Game.state = GameState.GameOver;
-        console.log("3.1");
         return;
     }
 
-    //if the snake hits its body
+    //if the snake hits itself
     for (var i = 0; i < Snake.body.length - 1; i++) {
 
         let currentBlock = Snake.body[i];
@@ -200,7 +200,6 @@ function checkIfSnakeDies() {
             && Snake.headPosition.y === currentBlock.y) {
 
             Game.state = GameState.GameOver;
-            console.log("3.2");
             return;
         }
     }
@@ -216,7 +215,6 @@ function clearCanvas() {
 
 function drawSnake() {
     Game.context.fillStyle = 'black';
-    // debugger;
 
     let width = Game.blockSize.width + 1;
     let height = Game.blockSize.height + 1;
@@ -233,7 +231,6 @@ function drawSnake() {
         Game.context.strokeStyle = "darkblue";
         Game.context.strokeRect(x, y, width, height);
     }
-    console.log("4");
 }
 
 function placeFood() {
@@ -250,47 +247,30 @@ function placeFood() {
                 x: foodX,
                 y: foodY
             }
-
-            console.log("food Place - 5");
         }
-
-
-        // for (let i = 0; i < Snake.body.length; i++) {
-        //     if (Snake.body[i].x !== foodX && Snake.body[i].y !== foodY) {
-        //         Game.food = {
-        //             x: foodX,
-        //             y: foodY
-        //         }
-        //         console.log("food Place - 5");
-        //     }
-        // }
     }
 }
 
 function drawFood() {
-    // Game.context.drawImage(foodImage, 26, 26, 50, 30);
-    // console.log(Game.food.x);
-    // console.log(Game.food.y);
-
-    Game.context.fillStyle = 'red';
+    
     let width = Game.blockSize.width + 1;
     let height = Game.blockSize.height + 1;
     let x = Game.food.x * (width + 1);
     let y = Game.food.y * (height + 1);
 
-    Game.context.fillRect(x, y, width, height);
-    console.log("draw food - 6");
+    Game.context.drawImage(foodImage, x, y, width, height);
 }
 
+//If the snake hits an apple, we adjust the score and clear the food coordinates
 function chechIfFoodWasEaten() {
     if (Snake.headPosition.x === Game.food.x
         && Snake.headPosition.y === Game.food.y) {
+
         Game.score += 1;
         Game.food = undefined;
 
         // duplicate block at tail of snake
         Snake.body.unshift(Snake.body[0]);
         console.log("7.2 - add food");
-
     }
 }
